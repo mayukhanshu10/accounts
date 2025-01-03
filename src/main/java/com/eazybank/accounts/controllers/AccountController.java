d09d584ab80d6403360e2602a9b4325250b4f6a5
Mayukh Srivastava <51943527+mayukhanshu10@users.noreply.github.com>
2025-01-03 22:08:16 +0530

    Added Swagger Annotations

--- src/main/java/com/eazybank/accounts/controllers/AccountController.java
+++ src/main/java/com/eazybank/accounts/controllers/AccountController.java
@@ -5,6 +5,9 @@ import com.eazybank.accounts.dtos.CustomerDto;
 import com.eazybank.accounts.dtos.ResponseDto;
 import com.eazybank.accounts.exceptions.ResourceNotFoundException;
 import com.eazybank.accounts.services.AccountService;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
@@ -13,6 +16,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(
+        name="CRUD Rest API for Accounts in EAZYBANK",
+        description = "CRUD APIs"
+)
 @RestController
 @RequestMapping(path="/api")
 /*@RequestMapping(path="/api", produces = (MediaType.APPLICATION_JSON_VALUE))
@@ -25,6 +32,10 @@ public class AccountController {
 
     private AccountService accountService;
 
+    @ApiResponse(
+            responseCode = "201",
+            description = "HTTP Status OK"
+    )
     @PostMapping("/create")
     public ResponseEntity<ResponseDto> createAccount(@Valid @RequestBody CustomerDto customerDto){
         accountService.createAccount(customerDto);
@@ -33,7 +44,10 @@ public class AccountController {
                 .body(new ResponseDto(AccountConstants.STATUS_201,AccountConstants.MESSAGE_201));
     }
 
-
+    @ApiResponse(
+            responseCode = "201",
+            description = "HTTP Status OK"
+    )
     @GetMapping("/fetch")
     public ResponseEntity<CustomerDto> fetchAccountDetails(@RequestParam
                                                                @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digit")
@@ -43,7 +57,16 @@ public class AccountController {
                 .status(HttpStatus.OK)
                 .body(customerDto);
     }
-
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error"
+            )
+    })
     @PutMapping("/update")
     public ResponseEntity<ResponseDto> updateAccountDetails(@Valid @RequestBody CustomerDto customerDto) throws ResourceNotFoundException {
         boolean isUpdated = accountService.updateAccount(customerDto);
@@ -58,6 +81,16 @@ public class AccountController {
         }
     }
 
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error"
+            )
+    })
     @DeleteMapping("/delete")
     public ResponseEntity<ResponseDto> deleteAccountDetails(@RequestParam
                                                                 @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digit")
